a52689f013586d66f3fd1d18b1aa06c268e12ae8
Joan Romero <romerojoan1999@gmail.com>
2023-03-16 20:46:04 -0400

    add routin highlight in navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,12 +1,24 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	$: isEvaluation = $page.url.pathname == '/evaluation';
+	$: isContrast = $page.url.pathname == '/contrast';
+
+	function isRef(ref: boolean) {
+		if (ref) return 'navbar__link active';
+		else return 'navbar__link';
+	}
+</script>
+
 <header class="header">
 	<nav class="navbar container">
 		<a href="/"><span class="main-logo">WEBACCESS</span></a>
 		<ul class="navbar__list">
 			<li class="navbar__item">
-				<a href="evaluation"><span>Evaluation</span></a>
+				<a href="evaluation" class={isRef(isEvaluation)}><span>Evaluation</span></a>
 			</li>
 			<li class="navbar__item">
-				<a href="contrast"><span>Contrast</span></a>
+				<a href="contrast" class={isRef(isContrast)}><span>Contrast</span></a>
 			</li>
 		</ul>
 	</nav>
